6e362788c17156e4105810c99584443b8cef568d
balazs4 <balazs4web@gmail.com>
2015-06-14 20:55:34 +0200

    Add stop support

--- express/router.js
+++ express/router.js
@@ -4,10 +4,24 @@ var _ = require('lodash');
 var Radio = require('../lib/radio');
 var channels = require('../lib/channels');
 var songparser = require('../lib/songparser');
+var notifier = require('../lib/')
+
 
 var radio = null;
 
+var stop = function(rdio) {
+    if (rdio != null) {
+        rdio.stop();
+        rdio = null;
+    }
+};
+
 var router = function(app) {
+
+    app.get('/stop', function(req, res)) {
+        stop(radio);
+        res.end();
+    };
 
     app.get('/channel', function(req, res) {
         res.json(channels);
@@ -21,10 +35,7 @@ var router = function(app) {
             return ch._id == id;
         });
 
-        if (radio != null) {
-            radio.stop();
-            radio = null;
-        }
+        stop(radio);
 
         radio = new Radio(channel.url);
         radio.on('playing', function(info) {
