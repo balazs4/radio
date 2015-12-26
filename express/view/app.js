f394d7bf94b7be14e0d3749934929630f50069f0
balazs4 <balazs4web@gmail.com>
2015-12-26 17:42:11 +0100

    Dummy UI (...)

--- express/view/app.js
+++ express/view/app.js
@@ -1,1 +1,44 @@
-console.log("hey");
+//var angular = require('angular');
+//var ngMaterial = require('angular-material');
+
+import 'angular-material/angular-material.css!';
+import angular from 'angular';
+import ngMaterial from 'angular-material';
+
+angular
+	.module('RadioApp', [ngMaterial])
+	.factory('RadioSrv', ['$http', ($http) => {
+		var radio = {};
+		radio.getChannels = function () {
+			return [{
+				"name": "1.FM High Voltage",
+				"url": "http://205.164.62.20:8035",
+				"cover": "http://www.asenation.com/images/1fm.png"
+			}, {
+				"name" : "StarFM From Hell",
+					"url": "http://87.230.53.43:7000",
+					"cover": "http://static.radio.de/images/broadcasts/c6/10/6615/c175.png"
+				}, {
+					"name" : "StarFM",
+					"url": "http://87.230.53.43:8004",
+					"cover": "http://i.img.co/radio/45/19/1945_145.png"
+				}, {
+					"name" : "Rockradio.com Metalcore",
+					"url": "http://pub10am.rockradio.com:80/rr_metalcore",
+					"cover": "http://static.radio.de/images/broadcasts/59/89/15093/c175.png"
+				}, {
+					"name" : "Drum N Bass Heaven",
+					"url": "http://91.121.138.222:8000",
+					"cover": "http://static.radio.net/images/broadcasts/40/42/14809/c175.png"
+				}];
+		}
+		return radio;
+	}])
+	.controller("MainCtrl", ['$scope', 'RadioSrv', ($scope, radio) => {
+		console.log("MainCtrl initialized..");
+		$scope.channels = radio.getChannels();
+	}]);
+
+angular.element(document).ready(() => {
+	angular.bootstrap(document, ['RadioApp']);
+});
--- express/view/config.js
+++ express/view/config.js
@@ -1,13 +1,56 @@
 System.config({
   baseURL: "./",
   defaultJSExtensions: true,
-  transpiler: "traceur",
+  transpiler: "false",
   paths: {
-    "github:*": "express/view/jspm_packages/github/*"
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   },
 
   map: {
+    "angular": "github:angular/bower-angular@1.4.8",
+    "angular-material": "github:angular/bower-material@1.0.1",
+    "css": "github:systemjs/plugin-css@0.1.20",
+    "false": "npm:babel-core@5.8.34",
+    "false-runtime": "npm:babel-runtime@5.8.34",
     "traceur": "github:jmcriffey/bower-traceur@0.0.93",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93"
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93",
+    "github:angular/bower-angular-animate@1.4.8": {
+      "angular": "github:angular/bower-angular@1.4.8"
+    },
+    "github:angular/bower-angular-aria@1.4.8": {
+      "angular": "github:angular/bower-angular@1.4.8"
+    },
+    "github:angular/bower-material@1.0.1": {
+      "angular": "github:angular/bower-angular@1.4.8",
+      "angular-animate": "github:angular/bower-angular-animate@1.4.8",
+      "angular-aria": "github:angular/bower-angular-aria@1.4.8",
+      "css": "github:systemjs/plugin-css@0.1.20"
+    },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:babel-runtime@5.8.34": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    }
   }
 });
