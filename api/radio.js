068cac01f8322dbe31e67b224a9824c1a7782d69
balazs4 <balazs4web@gmail.com>
2016-12-10 22:49:33 +0100

    add better unsub handling

--- api/radio.js
+++ api/radio.js
@@ -1,7 +1,6 @@
 const radio = require('express').Router();
-module.exports = radio
-
-const player = require('../lib/player');
+const player$ = require('../lib/player');
+const rx = require('rxjs');
 
 const channels = [
     "http://87.230.53.43:7000",
@@ -9,24 +8,27 @@ const channels = [
     "http://91.121.138.222:8000"
 ]
 
+let stream = rx.Observable.empty().subscribe(_ => { });
 
 radio.use('/:id', (req, res, next) => {
     req['channel'] = channels[req.params['id']]
     next();
 })
-radio.get('/:id', (req, res) => {
-    const channel = req['channel'];
-    res.json(channel)
-});
-radio.mkactivity('/:id', (req, res) => {
-    const channel = req['channel'];
+radio.get('/:id', (req, res) => res.json(req['channel']));
 
+radio.mkactivity('/:id', (req, res) => {
+    stream.unsubscribe();
+    stream = player$(req['channel'])
+        .retry(3)
+        .subscribe(song => console.log(new Date().toString(), song));
+    res.sendStatus(204);
 });
 
 radio.mkactivity('/', (req, res) => {
-
+    stream.unsubscribe();
+    res.sendStatus(204)
 })
 
-radio.get('/', (req, res) => {
-    res.json(channels)
-});
+radio.get('/', (req, res) => res.json(channels));
+
+module.exports = radio
--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const player$ = require('./lib/player');
-
-player$("http://87.230.53.43:7000")
-    .subscribe(x => console.log(x));
-
-
-// const app = require('express')();
-
-// app.use('/api/radio/', require('./api/radio'));
-// app.get('/', (req, res) => {
-//     res.send("Hello");
-// });
-
-// app.listen(4747);
+const app = require('express')();
+app.use('/api/radio/', require('./api/radio'));
+app.get('/', (req, res) => res.send("Hello"));
+app.listen(4747);
