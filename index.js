41e79d8b4e9a84f7ad102ec2edd20d6a0950c656
balazs4 <balazs4web@gmail.com>
2016-12-10 20:56:32 +0100

    player.js is now pure rxjs stream

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const player = require('./lib/player');
+const player$ = require('./lib/player');
 
-player("http://87.230.53.43:7000")
-.subscribe(x => {
-    console.log(x);
-})
-
+player$("http://87.230.53.43:7000")
+    .subscribe(x => console.log(x));
 
 
 // const app = require('express')();
