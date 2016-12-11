3967c8a5a2153406a91e63132efe1ace8ddaa792
balazs4 <balazs4web@gmail.com>
2016-12-11 16:11:39 +0100

    Split up source$ observables

--- api/radio.js
+++ api/radio.js
@@ -1,9 +1,9 @@
 const radio = require('express').Router();
 const rx = require('rxjs');
-const {get, post} = require('axios');
-const {encode} = require('node-base64-image');
+const {post} = require('axios');
 
 const player$ = require('../lib/player');
+const info$ = require('../lib/info');
 
 const channels = [
     "http://87.230.53.43:7000",
@@ -23,33 +23,11 @@ radio.mkactivity('/:id', (req, res) => {
     stream.unsubscribe();
     stream = player$(req['channel'])
         .filter(meta => !meta.StreamTitle.startsWith('NEXT: '))
-        .map(({StreamTitle, when, radio}) => {
+        .map(({StreamTitle, when, source}) => {
             const [artist, title] = StreamTitle.split(' - ');
-            return { artist, title, when, source: radio };
+            return { artist, title, source, when };
         })
-        .concatMap(song => rx.Observable
-            .of(song)
-            .map(({ artist, title }) => `${artist}+${title}`.replace(' ', '+'))
-            .switchMap(term => rx.Observable
-                .fromPromise(get(`https://api.spotify.com/v1/search?type=track&market=DE&limit=1&q=${term}`))
-                .map(response => response.data['tracks']['items'])
-                .map(items => items.length == 0
-                    ? { name: '' }
-                    : items[0]['album']
-                )
-                .map(({name, images}) => ({
-                    name,
-                    url: images && images.length > 0
-                        ? images[0]['url']
-                        : 'http://icons.iconarchive.com/icons/elegantthemes/beautiful-flat/128/radio-icon.png'
-                })
-                )
-            )
-            .switchMap(({name, url}) => rx.Observable
-                .bindNodeCallback(encode)(url, { string: true })
-                .map(x => Object.assign({}, song, { album: name, cover: x }))
-            )
-        )
+        .concatMap(info$)
         .do(song => console.log(Object.assign({}, song, { cover: '...' })))
         .subscribe(song => {
             post('http://ultrabook:4444/radio', song).catch(_ => { })
