6ab0389de19f5bdad6877cabf47fe90f3796900f
balazs4 <balazs4web@gmail.com>
2020-09-09 09:36:35 +0200

    feat: opml-to-json cli
    todo: rename repo

--- bin/radio.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-const { search } = require('..');
-const tocsv = x => x.map(y => `${y.url}\t${y.text}`).join('\n');
-(async () => {
-  const term = process.argv.slice(2).join('+');
-  const result = await search(term)
-  console.log(tocsv(result));
-})();
--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-const request = require('request-promise-native');
-const opmltojs = opml =>
-  new Promise(resolve => {
-    require('opmltojs').parse(opml, data => {
-      const raw = JSON.stringify(data, null, 2);
-      resolve(JSON.parse(raw));
-    });
+#! /usr/bin/env node
+
+const readline = require('readline');
+const opmltojson = require('opml-to-json');
+
+const std = readline.createInterface({
+  input: process.stdin,
+});
+
+const lines = [];
+std.on('line', (line) => lines.push(line));
+std.on('close', () => {
+  const input = lines.join('\n');
+  opmltojson(input, (err, parsed) => {
+    if (err) throw err;
+    console.log(JSON.stringify(parsed));
   });
-
-const search = async term => {
-  const opml = await request(
-    `http://opml.radiotime.com/Search.ashx?query=${term}`
-  );
-  const json = await opmltojs(opml);
-  const data = json['opml']['body']['subs']
-    .filter(x => x.type === 'audio')
-    .filter(x => x.item === 'station')
-    .map(({ text, URL, image, reliability = 0 }) => ({
-      text,
-      image,
-      score: parseInt(reliability),
-      url: URL
-    }))
-    .sort((a, b) => b.score - a.score);
-  return data;
-};
-
-module.exports = {
-  search
-};
+});
